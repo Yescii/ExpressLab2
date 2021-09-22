de4193e36a34eadfd16d5af3f56f6b3e4441bbbe
Yescii <cgonzalez.jesse@gmail.com>
2021-09-22 08:46:21 -0400

    Magic 8 ball express application created.

--- server.js
+++ server.js
@@ -12,6 +12,38 @@ app.get("/tip/:total/:tip", (req, res) => {
   res.send("Your tip amount is " + amount);
 });
 
+let qoute = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+app.get("/magic/:trick", (req, res) => {
+  let rndmNum = Math.floor(Math.random() * (qoute.length - 1) + 1);
+  let input = String(req.params.trick);
+
+  if (input == "Will I Be A Millionaire") {
+    res.send(`<h1>${qoute[rndmNum]} </h1>`);
+  }
+});
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
